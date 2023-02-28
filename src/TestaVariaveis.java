f6ff18f0768b3d26b711af7e2a2618bb7d3975a0
Camila Zucchi <camilazucchi@outlook.com>
2023-02-28 14:54:02 -0300

    feat: add int idade

--- src/TestaVariaveis.java
+++ src/TestaVariaveis.java
@@ -1,3 +1,10 @@
 public class TestaVariaveis {
+    public static void main(String[] args) {
+        System.out.println("Olá, novo teste.");
 
+        int idade = 25;
+        idade = 30 + 10;
+
+        System.out.println(idade);
+    }
 }
